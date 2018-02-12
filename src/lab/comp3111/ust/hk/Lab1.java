ebea955096cc3a850dccf44927473c29087c6cb1
cys_2 <cys_2@10.89.239.241>
2018-02-12 16:30:30 +0800

    Added a congratulation message

--- src/lab/comp3111/ust/hk/Lab1.java
+++ src/lab/comp3111/ust/hk/Lab1.java
@@ -3,5 +3,6 @@ package lab.comp3111.ust.hk;
 public class Lab1 {
 	public static void main(String[] args) {
 		System.out.println("Lab1: Introduction to Git and GitHub");
+		System.out.println("Congratulation! You learned the Git Basics");
 	}
 }
